462691b46686bdbbc8d20ca173b12a860715e9f6
Masadi Swetha <swetharaomasadi@gmail.com>
2025-03-04 18:18:09 +0530

    Updates

---
+++ precache-manifest.f53b100e115d92657e7b49be86edc8ac.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f047f9920f7828a693c8636507ce0c7e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b06b675f8365dbbcde4d",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "be4f7687718361ba3bfb",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "b8f22caa7a8f3a80d076",
+    "url": "/portfolio/static/css/main.2dfd5390.chunk.css"
+  },
+  {
+    "revision": "b67269b4baeeec4079d0",
+    "url": "/portfolio/static/js/2.46511cf8.chunk.js"
+  },
+  {
+    "revision": "a6c0177ea3999624a9f2d92abf620885",
+    "url": "/portfolio/static/js/2.46511cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06b675f8365dbbcde4d",
+    "url": "/portfolio/static/js/3.32eb430a.chunk.js"
+  },
+  {
+    "revision": "be4f7687718361ba3bfb",
+    "url": "/portfolio/static/js/4.6f5ae911.chunk.js"
+  },
+  {
+    "revision": "b8f22caa7a8f3a80d076",
+    "url": "/portfolio/static/js/main.e5c31d55.chunk.js"
+  },
+  {
+    "revision": "af6653b7cffe9fa5cb0f",
+    "url": "/portfolio/static/js/runtime-main.9bd7da8a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0a9fbc503ac58bc1fc0c0eb3b78bf75c",
+    "url": "/portfolio/static/media/aicte.0a9fbc50.png"
+  },
+  {
+    "revision": "c84924d8632dee97f32248d9962a213a",
+    "url": "/portfolio/static/media/aws.c84924d8.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "9bb3d71081f1e0638eb1f36202bdc873",
+    "url": "/portfolio/static/media/ey.9bb3d710.png"
+  },
+  {
+    "revision": "e708c32ff134867e1818be583a4da94b",
+    "url": "/portfolio/static/media/harvardLogo.e708c32f.png"
+  },
+  {
+    "revision": "f472f3129f539dd3ee18d49bb3cb287d",
+    "url": "/portfolio/static/media/image.f472f312.jpg"
+  },
+  {
+    "revision": "be7bac3948628d586bd85d6dddefd071",
+    "url": "/portfolio/static/media/newkakatiya.be7bac39.png"
+  },
+  {
+    "revision": "918fa6fb15667730d6fc9222f2bd0fc7",
+    "url": "/portfolio/static/media/nit.918fa6fb.png"
+  },
+  {
+    "revision": "85765f0e40ccada12268a97c4fe2d4a9",
+    "url": "/portfolio/static/media/nptel.85765f0e.png"
+  },
+  {
+    "revision": "15df3aa0f7439f6a179053bace9cd27e",
+    "url": "/portfolio/static/media/resume.15df3aa0.pdf"
+  },
+  {
+    "revision": "f8f7e5daf41695fe4c69cb270f3e35a2",
+    "url": "/portfolio/static/media/shivani.f8f7e5da.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "1dc43b2196c580111229114c3c5f1096",
+    "url": "/portfolio/static/media/srm.1dc43b21.png"
+  },
+  {
+    "revision": "eba52e559cb10c41effea234a63714f2",
+    "url": "/portfolio/static/media/unstop.eba52e55.png"
+  }
+]);
